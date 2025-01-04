8c68599557f66f9de461ad3ea5007b5616770757
Frank Luongo <frank@frankluongo.com>
2025-01-04 16:25:16 -0500

    fix: add layouts

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,30 +1,10 @@
-import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
 const HomePage = () => {
   return (
     <>
       <Metadata title="Home" description="Home page" />
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>Home</main>
-
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.jsx</code>
-      </p>
-      {/*
-           My default route is named `home`, link to me with:
-           `<Link to={routes.home()}>Home</Link>`
-        */}
+      Home
     </>
   )
 }
