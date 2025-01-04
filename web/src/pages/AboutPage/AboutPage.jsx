604ef7b5eaadd37d3b04cba35fef5f57373e77c7
Frank Luongo <frank@frankluongo.com>
2025-01-04 16:19:49 -0500

    fix: update pages

--- web/src/pages/AboutPage/AboutPage.jsx
+++ web/src/pages/AboutPage/AboutPage.jsx
@@ -1,4 +1,4 @@
-// import { Link, routes } from '@redwoodjs/router'
+import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
 const AboutPage = () => {
@@ -6,14 +6,23 @@ const AboutPage = () => {
     <>
       <Metadata title="About" description="About page" />
 
-      <h1>AboutPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/AboutPage/AboutPage.jsx</code>
-      </p>
-      {/*
-           My default route is named `about`, link to me with:
-           `<Link to={routes.about()}>About</Link>`
-        */}
+      <header>
+        <h1>Redwood Blog</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <p>
+          This site was created to demonstrate my mastery of Redwood: Look on my
+          works, ye mighty, and despair!
+        </p>
+        <Link to={routes.home()}>Return home</Link>
+      </main>
     </>
   )
 }
--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,21 @@
-// import { Link, routes } from '@redwoodjs/router'
+import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
 const HomePage = () => {
   return (
     <>
       <Metadata title="Home" description="Home page" />
+      <header>
+        <h1>Redwood Blog</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>Home</main>
 
       <h1>HomePage</h1>
       <p>
